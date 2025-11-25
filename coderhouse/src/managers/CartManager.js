96e8b0df05601dfe539960a78f22b79ce654f5c7
chrisherrero <chris-herre@hotmail.com>
2025-11-25 02:35:05 -0300

    Clean up comments in CartManager methods
    Removed commented lines in createCart and addProduct methods for cleaner code.

--- coderhouse/src/managers/CartManager.js
+++ coderhouse/src/managers/CartManager.js
@@ -19,7 +19,7 @@ class CartManager {
 
     async createCart() {
         const carts = await this.getCarts();
-        // ID autogenerado
+   
         const id = carts.length > 0 ? carts[carts.length - 1].id + 1 : 1;
         
         const newCart = {
@@ -42,14 +42,14 @@ class CartManager {
         const cartIndex = carts.findIndex(c => c.id === cartId);
 
         if (cartIndex !== -1) {
-            // Ver si el producto ya existe en el carrito
+            
             const productIndex = carts[cartIndex].products.findIndex(p => p.product === productId);
 
             if (productIndex !== -1) {
-                // Si existe, sumamos quantity
+            
                 carts[cartIndex].products[productIndex].quantity++;
             } else {
-                // Si no, lo agregamos con quantity: 1
+             
                 carts[cartIndex].products.push({ product: productId, quantity: 1 });
             }
 
